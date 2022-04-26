7cca0af5c6af36a387533dbe472307f926f17138
thi-phng <thi-phng@student.42.fr>
2022-04-26 12:33:38 +0200

    main.run in commit

--- C00/ex01/main.cpp
+++ C00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/18 18:19:42 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/04/23 15:55:15 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/04/26 12:29:40 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,14 @@
 
 int		main(int ac, char **av)
 {
+    PhoneBook   run;
+    
 	(void)av;
 	if (ac == 1)
 	{
 		std::cout << "Ok Argument but the PhoneBook is not done yet, pls come back later hihi" << std::endl;
 		//do something
+        //run.open();
 		return (0);
 	}
 	std::cout << "Invalid Arguments" << std::endl;
