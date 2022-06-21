1409d177475e63557619818f8f42cce83553feb9
thi-phng <thi-phng@student.42.fr>
2022-06-21 12:28:35 +0200

    creating ex03

--- C01/ex03/HumanA.hpp
+++ C01/ex03/HumanA.hpp
@@ -3,11 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   HumanA.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thi-phng <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 20:51:42 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/06/20 20:51:45 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/06/21 12:27:02 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
+#ifndef HUMANA_HPP
+# define HUMANA_HPP
+
+class   HumanA
+{
+    public:
+    //a waepon
+    //and a name
+        HumanA(void);
+        ~HumanA();
+        std::string name;
+
+        std::string attack()
+};
+
+
+#endif
--- C01/ex03/Weapon.hpp
+++ C01/ex03/Weapon.hpp
@@ -3,11 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   Weapon.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thi-phng <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 20:51:11 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/06/20 20:51:12 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/06/21 12:25:39 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 
+class   Weapon
+{
+    public:
+        Weapon(void);
+        ~Weapon();
+        std::string const getType();
+        std::string setType(std::string const New);
+
+    private:
+        std::string     _type;
+};
+
+std::string const getType()
+{
+    Weapon::_type New;
+    
+    std::string &typeRef = New;
+    return (New);
+}
+
+std::string setType(std::string const New)
+{
+    //euh now what???
+}
