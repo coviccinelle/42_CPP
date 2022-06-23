1d1d907fc707f975df0a4cf2f13fb54b7eae7d32
thi-phng <thi-phng@student.42.fr>
2022-06-23 12:32:33 +0200

    gitignore vs changed ex01 -> memory adress held by stringPTR

--- C01/ex02/main.cpp
+++ C01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 20:42:48 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/06/21 11:41:12 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/06/23 12:28:35 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int		main()
 	std::string	&stringRef = Str; //Ref is pretty much an alias of Str
 
 	std::cout << "The memory address of the string variable is : " << &Str << std::endl;
-	std::cout << "The memory address held by stringPTR is : " << &stringPtr << std::endl;
+	std::cout << "The memory address held by stringPTR is : " << stringPtr << std::endl;
 	std::cout << "The memory address held by stringREF is : " << &stringRef << std::endl;
 	
 	std::cout << "Wait! It's not done yet..." << std::endl << std::endl;
