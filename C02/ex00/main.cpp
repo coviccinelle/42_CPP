108102e1597caf4c4ad98314da798c589fdaab2d
thi-phng <thi-phng@student.42.fr>
2022-06-25 18:28:53 +0200

    setting files and headers

---
+++ C02/ex00/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/25 18:12:00 by thi-phng          #+#    #+#             */
+/*   Updated: 2022/06/25 18:21:48 by thi-phng         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main( void ) {
+
+Fixed a;
+Fixed b( a );
+Fixed c;
+
+c = b;
+
+std::cout << a.getRawBits() << std::endl;
+std::cout << b.getRawBits() << std::endl;
+std::cout << c.getRawBits() << std::endl;
+
+return 0;
+}
