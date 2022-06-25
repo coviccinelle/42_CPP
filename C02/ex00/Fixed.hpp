9912e5b78eca3da93396f4ea30d0812f458d0c12
thi-phng <thi-phng@student.42.fr>
2022-06-25 18:39:30 +0200

    starting ex00, not doing much

--- C02/ex00/Fixed.hpp
+++ C02/ex00/Fixed.hpp
@@ -3,11 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   Fixed.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thi-phng <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 18:12:18 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/06/25 18:12:20 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/06/25 18:38:57 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
+#include <iostream>
+
+class Fixed
+{
+private:
+    int                 _FixedPointNumber;
+    static const int    // number of fractional bits
+    /* data */
+public:
+    Fixed(/* args */);
+    Fixed();
+    ~Fixed();
+};
+
+Fixed::Fixed(/* args */)
+{
+}
+
+Fixed::~Fixed()
+{
+}
