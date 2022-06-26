c669050db95a4ab6f83019f2795ffaf38bc200e0
thi-phng <thi-phng@student.42.fr>
2022-06-26 13:03:20 +0200

    Fixed.hpp added ifndef

--- C02/ex00/Fixed.hpp
+++ C02/ex00/Fixed.hpp
@@ -6,12 +6,14 @@
 /*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 18:12:18 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/06/25 18:38:57 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/06/26 13:02:53 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FIXED_HPP
+# define FIXED_HPP
 
-#include <iostream>
+# include <iostream>
 
 class Fixed
 {
@@ -31,4 +33,6 @@ Fixed::Fixed(/* args */)
 
 Fixed::~Fixed()
 {
-}
+}i
+
+#endif
