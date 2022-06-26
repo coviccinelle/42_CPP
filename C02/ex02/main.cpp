a0a4cf03637e79a22e06bfcf7d6df3c5567f3399
thi-phng <thi-phng@student.42.fr>
2022-06-26 21:47:03 +0200

    adding main ex02

--- C02/ex02/main.cpp
+++ C02/ex02/main.cpp
@@ -3,11 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thi-phng <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 18:12:00 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/06/25 18:12:02 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/06/26 21:46:12 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
+#include <iostream>
+
+int main( void ) {
+    
+Fixed a;
+Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+
+std::cout << a << std::endl;
+std::cout << ++a << std::endl;
+std::cout << a << std::endl;
+std::cout << a++ << std::endl;
+std::cout << a << std::endl;
+
+std::cout << b << std::endl;
+
+std::cout << Fixed::max( a, b ) << std::endl;
+
+return 0;
+}
