c70cee85e36ade4c393e3c8d900d921f9279db70
thi-phng <thi-phng@student.42.fr>
2022-06-26 21:47:52 +0200

    adding ex02 hpp

--- C02/ex02/Fixed.hpp
+++ C02/ex02/Fixed.hpp
@@ -3,11 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   Fixed.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thi-phng <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 18:12:18 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/06/25 18:12:20 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/06/26 21:47:23 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+
+
+
+#endif
