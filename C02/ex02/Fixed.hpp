b45b53c95016e8296d92125050074c9ed02a47d3
thi-phng <thi-phng@student.42.fr>
2022-07-04 19:03:50 +0200

    coppying ex01 into ex02

--- C02/ex02/Fixed.hpp
+++ C02/ex02/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 18:12:18 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/07/04 18:52:37 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/07/04 19:03:42 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,16 +57,16 @@ class Fixed
 
         Fixed   & operator++(void);             //++a;
         Fixed   & operator--(void);             //--a;
-        Fixed   operator++(void);               //a++;
-        Fixed   operator--(void);               //a--;
+        Fixed   operator++(int);                //a++;
+        Fixed   operator--(int);                //a--;
         
 
 
         //***----- [ The 4 public overloaded member functions ] ----***//
-        static Fixed            & min(&);
+        static Fixed            & min(Fixed & fx1, Fixed );
         static Fixed            & min(const );
-        static Fixed            max(&);
-        static max(const )
+        static Fixed            & min(&);
+        static Fixed            & max(const )
         
         
         float   toFloat(void) const;
