32752f8c49485dbf7898ed209612d59708be97d6
thi-phng <thi-phng@student.42.fr>
2022-07-04 19:25:22 +0200

    adding include header

--- C02/ex02/main.cpp
+++ C02/ex02/main.cpp
@@ -6,11 +6,11 @@
 /*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 18:12:00 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/07/04 19:22:39 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/07/04 19:23:37 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#incude "Fixed.hpp"
+#include "Fixed.hpp"
 #include <iostream>
 
 int main( void ) {
