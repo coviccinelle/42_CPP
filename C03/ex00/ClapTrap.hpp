d9fafae4751287c5be01a7703ee675eb933e6811
thi-phng <thi-phng@student.42.fr>
2022-07-05 11:50:16 +0200

    building ClapTrap hpp

--- C03/ex00/ClapTrap.hpp
+++ C03/ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 18:12:18 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/07/05 11:16:40 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/07/05 11:49:49 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,28 +19,26 @@
 
 class ClapTrap
 {
-    private:
-        int                 _FixedPointNumber;
-        static int const    _Number_bits = 8;// number of fractional bits
-        /* data */
-        
+    
     public:
-        ClapTrap(void);                //canonical default constructeur
-        ClapTrap(const int n);
-        ClapTrap(const float f);
-        ClapTrap(ClapTrap const & fx);    //canonical copy constructor
-        ~ClapTrap(void);               //canonical default destructor
+        ClapTrap(void);                                 //canonical default constructeur
+        ClapTrap(ClapTrap const & fx);                  //canonical copy constructor
+        ~ClapTrap(void);                                //canonical default destructor
+        ClapTrap & operator= ( const ClapTrap & fx);    //canonical copy assignment operator overload
 
-        float   toFloat(void) const;
-        int     toInt(void) const;
-        ClapTrap & operator= ( const ClapTrap & fx); //canonical copy assignment operator overload
-        int     getRawBits (void) const;        //return the raw value of the fixed-point value
-        void    setRawBits (int const raw);      //set the raw value of the fixed-point number
+        attack(const std::strng & target);
+        takeDamage(unsigned int amount);
+        beRepaired(unsigned int amount);
         
-
+    private:
+        ClapTrap(std::string    Name);
+        int                     _HitPoints = 10;
+        int                     _EnergyPoints = 10;
+        int                     _AttackDamage = 0;
+        
 };
 
-std::ostream & operator << (std::ostream & sortie, const ClapTrap & fx);
+//std::ostream & operator << (std::ostream & sortie, const ClapTrap & fx);
 // An overload of the insertion («) operator that inserts a floating-point representation
 //of the fixed-point number into the output stream object passed as parameter
 
