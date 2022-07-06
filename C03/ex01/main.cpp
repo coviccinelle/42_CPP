8ccdd10f3bf7e6819f6b54e1f28d3f7821d2c094
thi-phng <thi-phng@student.42.fr>
2022-07-06 16:10:19 +0200

    kind of done ex01

--- C03/ex01/main.cpp
+++ C03/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 18:12:00 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/07/06 13:30:20 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/07/06 15:11:05 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,20 @@
 int main(void)
 {
 	ScavTrap scav("Tom 🐱 ");
-	std::cout << "-------------- We create the Instance ------------------" << std::endl;
+	std::cout << "-------------- [ Instance created ] ------------------" << std::endl;
 	std::cout << scav << std::endl;
 
-	std::cout << "-------------- Attack function ------------------" << std::endl;
+	std::cout << "-------------- [ Attack function ] ------------------" << std::endl;
 	scav.attack("Jerry 🐭 ");
 	std::cout << scav << std::endl;
 	
 	
-	std::cout << "-------------- Dammage function ------------------" << std::endl;
+	std::cout << "-------------- [ Damage function ] ------------------" << std::endl;
 	scav.ClapTrap::takeDamage(12);
 	std::cout << scav << std::endl;
 
 
-	std::cout << "-------------- Guard Gate function ------------------" << std::endl;
+	std::cout << "-------------- [ Guard Gate function ] ------------------" << std::endl;
 	scav.guardGate();
 	std::cout << scav << std::endl;
 
