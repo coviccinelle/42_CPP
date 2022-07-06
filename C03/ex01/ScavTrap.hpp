2fdf34d5bc8a8bf4d9852c2d0bac5910a122ddf1
thi-phng <thi-phng@student.42.fr>
2022-07-06 10:51:44 +0200

    adding ScavTrap cpp and hpp

--- C03/ex01/ScavTrap.hpp
+++ C03/ex01/ScavTrap.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ScavTrap.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thi-phng <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 10:47:47 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/07/06 10:49:08 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/07/06 10:51:31 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,30 @@
 
 class ScavTrap
 {
+    public:
+        ClapTrap(void);                                 //canonical default constructeur
+        ClapTrap(ClapTrap const & fx);                  //canonical copy constructor
+        ClapTrap & operator= ( const ClapTrap & fx);    //canonical copy assignment operator overload
+		virtual ~ClapTrap(void);				        //canonical default destructor 
+                                                        //virtual avoid memory leaks in case of inheritance class
+        ClapTrap(std::string    Name);
+
+        void    attack(const std::string & target);
+        void    takeDamage(unsigned int amount);
+        void    beRepaired(unsigned int amount);
+		void	annonce(void)const;
+
+		std::string	    getName(void)const;
+		unsigned int    getHitp(void)const;
+		unsigned int	getEnergyp(void)const;
+		unsigned int	getAttackd(void)const;
+                
+    protected:
+        std::string     _Name;
+        unsigned int    _HitPoints;
+        unsigned int    _EnergyPoints;
+        unsigned int    _AttackDamage;
 
 };
-
 
 #endif
