c32006b4bfcc84084962f0d53b35ec893292b77b
thi-phng <thi-phng@student.42.fr>
2022-07-06 10:37:33 +0200

    Initialize the new ex01

--- C03/ex01/main.cpp
+++ C03/ex01/main.cpp
@@ -6,12 +6,13 @@
 /*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 18:12:00 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/07/06 10:17:10 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/07/06 10:37:24 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
 
 int main( void ) 
 {
@@ -20,52 +21,6 @@ int main( void )
 
     Fighter.annonce();
 
-    std::cout  <<  std::endl;
-	Fighter.attack("Jerry");
-	Fighter.annonce();
-    
-    std::cout  <<  std::endl;
-	Fighter.beRepaired(2);
-	Fighter.annonce();
-
-    std::cout  <<  std::endl;
-	Fighter.takeDamage(15);
-	Fighter.annonce();
-	
-    std::cout  <<  std::endl;
-	Fighter.attack("Jerry");
-	Fighter.annonce();
-
-    std::cout  <<  std::endl;
-	Fighter.beRepaired(22);
-	Fighter.annonce();
-
-    std::cout  <<  std::endl;
-	Fighter.takeDamage(10);
-	Fighter.annonce();
-	
-	
-	Fighter.attack("Jerry");
-	Fighter.attack("Jerry");
-	Fighter.attack("Jerry");
-    Fighter.attack("Jerry");
-    Fighter.attack("Jerry");
-    Fighter.attack("Jerry");
-
-
-	std::cout << std::endl << "------------ Should not work anymore ------------------" << std::endl << std::endl;
-	
-	Fighter.attack("Jerry");
-	Fighter.annonce();
-
-	std::cout  <<  std::endl;
-	Fighter.takeDamage(15);
-	Fighter.annonce();
-
-	std::cout << "------------ Should not work anymore ------------------" << std::endl;
-	std::cout  <<  std::endl;
-	Fighter.takeDamage(15);
-	Fighter.annonce();
 	
 	return (0); 
 }
