f5624355750226a1db0a3f0c80642d4b83907e90
thi-phng <thi-phng@student.42.fr>
2022-07-06 10:36:21 +0200

    Initialize the new ex01

---
+++ C03/ex01/ClapTrap.cpp
@@ -0,0 +1,144 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/25 18:12:30 by thi-phng          #+#    #+#             */
+/*   Updated: 2022/07/05 21:15:54 by thi-phng         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(void) : _Name("Default"), _HitPoints(10), _EnergyPoints(10), _AttackDamage(0)
+{
+    std::cout << "Default constructor called hihi" << std::endl;
+    return ;
+}
+
+
+ClapTrap::ClapTrap(std::string name) : _Name(name), _HitPoints(10), _EnergyPoints(10), _AttackDamage(0)
+{
+    std::cout << "Name constructor called" << std::endl;
+    return ;
+}
+
+ClapTrap::~ClapTrap()
+{
+    std::cout << "Destructor called" << std::endl;
+    return ;
+}
+
+
+ClapTrap::ClapTrap(ClapTrap const & fx)    //canonical copy constructor
+{
+    std::cout << "Copy constructor called" << std::endl;
+    *this = fx;
+    return ;
+}
+
+ClapTrap & ClapTrap::operator=(const ClapTrap & fx)//copy assignment operator overload
+{
+    // init values
+    std::cout << "Copy assignment operator called" << std::endl;
+
+    this->_Name = fx.getName();
+    this->_HitPoints = fx.getHitp();
+    this->_EnergyPoints = fx.getEnergyp();
+    this->_AttackDamage = fx.getAttackd();
+    
+    return *this;
+}
+
+
+//////////////////////////////////////////////////
+///				Action Functions	    	   ///
+//////////////////////////////////////////////////
+
+
+void    ClapTrap::attack(const std::string & target)
+{
+    if (this->_EnergyPoints > 0)
+    {
+        this->_EnergyPoints -= 1;
+        std::cout << "[Attack]: " << this->_Name << " attacks " << target << ", causing ";
+        std::cout << _AttackDamage << " points of damage!" << std::endl;
+    }
+    else
+        std::cout << "⚠️ 🚧 🚸 ERROR :: Can't attack: Not enough [Energy points]!" << std::endl;
+    return ;
+}
+
+
+void    ClapTrap::takeDamage(unsigned int amount)
+{
+    if (this->_HitPoints > 0)
+    {
+        if (amount > this->_HitPoints)
+            this->_HitPoints = 0;
+        else
+            this->_HitPoints -= amount;
+        std::cout << "[Take damage] : " << this->_Name << " takes " << amount << " of damage from the enemy"<< std::endl;
+    }
+    else
+        std::cout << "⚠️ 🚧 🚸 ERROR :: Can't take damage. Not enough [Hit points]! (You died already dude)" << std::endl;
+    return ;
+}
+
+void    ClapTrap::beRepaired(unsigned int amount)
+{
+    if (this->_EnergyPoints > 0)
+    {
+        this->_HitPoints += amount;
+        this->_EnergyPoints -= 1;
+        std::cout << "[beRepaired] " << this->_Name << " repaired " << amount << " points of HitPoints" << std::endl;
+    }
+    else
+        std::cout << "⚠️ 🚧 🚸 ERROR :: Can't repaire: Not enough [Energy points]!" << std::endl;
+    return ;
+}
+
+
+
+//////////////////////////////////////////////////
+///				Get Functions				   ///
+//////////////////////////////////////////////////
+
+std::string	ClapTrap::getName(void) const
+{
+	return (this->_Name);
+}
+
+unsigned int		ClapTrap::getHitp(void) const
+{
+	return (this->_HitPoints);
+}
+
+unsigned int		ClapTrap::getEnergyp(void) const
+{
+	return (this->_EnergyPoints);
+}
+
+unsigned int		ClapTrap::getAttackd(void) const
+{
+	return (this->_AttackDamage);
+}
+
+void	ClapTrap::annonce(void)const
+{
+	std::cout << std::endl;
+	std::cout << this->_Name << "'s Energy points = " << this->_EnergyPoints << std::endl;
+	std::cout << this->_Name << "'s Hit points = " << this->_HitPoints << std::endl;
+	std::cout << std::endl;
+}
+
+
+// // An overload of the insertion («) operator that inserts a floating-point representation
+// // of the fixed-point number into the output stream object passed as parameter.
+// std::ostream & operator << (std::ostream & cout, const ClapTrap & fx)
+// {
+// 	cout << fx.toFloat();
+// 	return (cout);
+// }
---
+++ C03/ex01/ClapTrap.hpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/25 18:12:18 by thi-phng          #+#    #+#             */
+/*   Updated: 2022/07/05 20:52:54 by thi-phng         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+# include <cmath>
+
+class ClapTrap
+{
+    
+    public:
+        ClapTrap(void);                                 //canonical default constructeur
+        ClapTrap(ClapTrap const & fx);                  //canonical copy constructor
+        ClapTrap & operator= ( const ClapTrap & fx);    //canonical copy assignment operator overload
+		virtual ~ClapTrap(void);				        //canonical default destructor 
+                                                        //virtual avoid memory leaks in case of inheritance class
+        ClapTrap(std::string    Name);
+
+        void    attack(const std::string & target);
+        void    takeDamage(unsigned int amount);
+        void    beRepaired(unsigned int amount);
+		void	annonce(void)const;
+
+		std::string	    getName(void)const;
+		unsigned int    getHitp(void)const;
+		unsigned int	getEnergyp(void)const;
+		unsigned int	getAttackd(void)const;
+                
+    private:
+        std::string     _Name;
+        unsigned int    _HitPoints;
+        unsigned int    _EnergyPoints;
+        unsigned int    _AttackDamage;
+};
+
+#endif
