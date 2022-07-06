63c8832aafca963976332fd69e743f09121f6e5b
thi-phng <thi-phng@student.42.fr>
2022-07-06 11:40:51 +0200

    add definition of protected

--- C03/ex01/ClapTrap.hpp
+++ C03/ex01/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 18:12:18 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/07/06 10:50:44 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/07/06 11:39:53 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,10 @@ class ClapTrap
 		unsigned int    getHitp(void)const;
 		unsigned int	getEnergyp(void)const;
 		unsigned int	getAttackd(void)const;
-                
+
+
+    // [protected] is very similar to a private member but it they can be accessed 
+    // in child classes which are called derived classes
     protected:
         std::string     _Name;
         unsigned int    _HitPoints;
