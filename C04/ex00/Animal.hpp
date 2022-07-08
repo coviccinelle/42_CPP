a74382999cc0765d45758c31b2fb8a5def864a3f
thi-phng <thi-phng@student.42.fr>
2022-07-08 11:44:18 +0200

    very first step of setting CPP04, ex00, adding WrongCat and WrongAnimal

--- C04/ex00/Animal.hpp
+++ C04/ex00/Animal.hpp
@@ -3,11 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   Animal.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thi-phng <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/08 11:12:43 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/07/08 11:12:45 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/07/08 11:41:50 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
 
+#include <iostream>
+
+class Animal
+{
+	public:
+		//Constructor
+		Animal(void);									//canonical constructeur par defaut
+		Animal(std::string type);
+		Animal(Animal const & rhs);						//canonical constructeur de recopie
+		
+		//Function
+		Animal			&operator= ( const Animal & rhs);	//canonical surcharge de l'operation d'affectation
+		std::string		getType(void)const;
+		void			setType(std::string type);
+		virtual void	makeSound(void)const;
+		
+		//Destructor
+		virtual ~Animal(void);							//canonical destructeur
+	
+	protected:
+	
+		std::string	_type;
+	
+	private:
+};
