660e7dd8a187108e4c94d743f9d956d3ebfc975a
thi-phng <thi-phng@student.42.fr>
2022-07-08 13:59:21 +0200

    done ex02

--- C03/ex02/main.cpp
+++ C03/ex02/main.cpp
@@ -6,42 +6,39 @@
 /*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 18:12:00 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/07/08 13:49:08 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/07/08 13:58:46 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "FragTrap.hpp"
-#include "ScavTrap.hpp"
+//#include "ScavTrap.hpp"
 
 int main(void)
 {
-	 //ScavTrap Scav("Tom 🐱 ");
+	// ScavTrap Scav("Tom 🐱 ");
 	// std::cout << "-------------- [ Instance created ] ------------------" << std::endl;
 	// std::cout << Scav << std::endl;
-	//std::ostream & operator << (std::ostream & cout, const FragTrap & fx)
-
-
+	// std::ostream & operator << (std::ostream & cout, const FragTrap & fx);
 
 	
 	FragTrap Frag("Frag Tom 🐱 ");
-	//FragTrap Frag;
 	std::cout << "-------------- [ Instance created ] ------------------" << std::endl;
 	std::cout << Frag << std::endl;
 	std::ostream & operator << (std::ostream & cout, const FragTrap & fx);
 	// -> Thanks to this function = announce function dude, magic right?
 
 	std::cout << "-------------- [ Attack function ] ------------------" << std::endl;
-	//Frag.attack("Jerry 🐭 ");
-	//std::cout << Frag << std::endl;
+	Frag.attack("Jerry 🐭 ");
+	std::cout << Frag << std::endl;
 	
 	
-	// std::cout << "-------------- [ Damage function ] ------------------" << std::endl;
-	// Frag.ClapTrap::takeDamage(12);
-	// std::cout << Frag << std::endl;
+	std::cout << "-------------- [ Damage function ] ------------------" << std::endl;
+	Frag.ClapTrap::takeDamage(12);
+	std::cout << Frag << std::endl;
 
 
 	std::cout << "-------------- [ High Five function ] ------------------" << std::endl;
-	//Frag.highFivesGuys();
+	Frag.highFiveGuys();
 	//std::cout << Frag << std::endl;
 
 	return (0);    
