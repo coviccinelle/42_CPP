c79b570d9e5a58a152185f7a4ed32e4b118c2d95
thi-phng <thi-phng@student.42.fr>
2022-07-09 11:01:35 +0200

    adding dog and cat stuff in animal

--- C04/ex00/Animal.hpp
+++ C04/ex00/Animal.hpp
@@ -6,7 +6,7 @@
 /*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/08 11:12:43 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/07/08 11:41:50 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/07/09 10:54:41 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,19 @@ class Animal
 {
 	public:
 		//Constructor
-		Animal(void);									//canonical constructeur par defaut
+		Animal(void);									//canonical default constructeur
 		Animal(std::string type);
-		Animal(Animal const & rhs);						//canonical constructeur de recopie
+		Animal(Animal const & rhs);						//canonical copy constructor
 		
 		//Function
-		Animal			&operator= ( const Animal & rhs);	//canonical surcharge de l'operation d'affectation
+		Animal			&operator= ( const Animal & rhs);	//canonical copy assignment operator overload
 		std::string		getType(void)const;
 		void			setType(std::string type);
 		virtual void	makeSound(void)const;
 		
 		//Destructor
-		virtual ~Animal(void);							//canonical destructeur
+		virtual ~Animal(void);////canonical default destructor (virtual avoid memory leaks in case of inheritance)
 	
 	protected:
-	
 		std::string	_type;
-	
-	private:
 };
--- C04/ex00/Dog.hpp
+++ C04/ex00/Dog.hpp
@@ -3,11 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   Dog.hpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thi-phng <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/08 11:13:22 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/07/08 11:13:23 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/07/09 10:59:40 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
 
+#include "Animal.hpp"
+
+class Dog : public Animal
+{
+    public:
+        Animal();
+        Animal(Animal const & fx);              //canonical copy constructor
+        Animal & operator= (const Animal & fx); //canonical copy assignment operator overload
+        ~Animal();
+        
+    private:
+    
+    
+};
