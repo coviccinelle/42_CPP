dd7f9bf21c963e868b25146a80811618a6fdc0fd
thi-phng <thi-phng@student.42.fr>
2022-07-11 15:02:59 +0200

    building Dog.cpp now

--- C04/ex00/Dog.cpp
+++ C04/ex00/Dog.cpp
@@ -3,11 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   Dog.cpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thi-phng <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/08 11:13:10 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/07/08 11:13:11 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/07/11 14:56:44 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+//#pragma once
 
+#include "Dog.hpp"
+
+Dog::Dog(void)                        //canonical default constructor
+{
+    std::cout << "[Dog]: Default constructor called" << std::endl;
+    return ;
+}
+
+
+Dog::Dog(Dog const & fx)              //canonical copy constructor
+{
+    std::cout << "[Dog]: Copy constructor called" << std::endl;
+    return ;
+}
+
+Dog & operator= (const Dog & fx) //canonical copy assignment operator overload
+{
+    
+}
+
+virtual ~Dog(void);                   //canonical destructor
+
+
+
+void    makeSound(void) const;
--- C04/ex00/main.cpp
+++ C04/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 18:12:00 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/07/09 19:51:30 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/07/11 14:49:45 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,35 +17,16 @@
 #include "WrongCat.hpp"
 #include "WrongAnimal.hpp"
 
-// int main( void ) 
-// {
-// 	Animal Fighter("Tom");
-// 	//std::cout  << Fighter.getName() << " was born" << std::endl;
-// 	std::cout << "One Animal was born" << std::endl;
-
-//     //Fighter.annonce();
-
-//     std::cout  <<  std::endl;
-// 	//Fighter.attack("Jerry");
-// 	//Fighter.annonce();
-    
-// 	std::cout << std::endl << "------------ Should not work anymore ------------------" << std::endl << std::endl;
-
-// 	std::cout << "------------ Should not work anymore ------------------" << std::endl;
-	
-// 	return (0); 
-// }
-
 int	main()
 {
-const Animal* meta = new Animal();
-const Animal* j = new Dog();
-//const Animal* i = new Cat();
-std::cout << j->getType() << " " << std::endl;
-//std::cout << i->getType() << " " << std::endl;
-//i->makeSound(); //will output the cat sound!
-j->makeSound();
-meta->makeSound();
-//...
-return 0;
+    const Animal* meta = new Animal();
+    const Animal* j = new Dog();
+    //const Animal* i = new Cat();
+    std::cout << j->getType() << " " << std::endl;
+    //std::cout << i->getType() << " " << std::endl;
+    //i->makeSound(); //will output the cat sound!
+    j->makeSound();
+    meta->makeSound();
+    //...
+    return 0;
 }
