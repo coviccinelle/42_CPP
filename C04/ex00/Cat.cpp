15fd6f80c83e5b363ec273c244010f9d620b1241
thi-phng <thi-phng@student.42.fr>
2022-07-11 15:57:41 +0200

    Done Cat.cpp i think

--- C04/ex00/Cat.cpp
+++ C04/ex00/Cat.cpp
@@ -3,11 +3,44 @@
 /*                                                        :::      ::::::::   */
 /*   Cat.cpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thi-phng <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/08 11:13:43 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/07/08 11:13:45 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/07/11 15:57:12 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
+#include "Cat.hpp"
+
+Cat::Cat(void) : Animal("Cat")                        //canonical default constructor
+{
+    std::cout << "[Cat]: Default constructor called" << std::endl;
+    return ;
+}
+
+Cat::Cat(Cat const & fx)              //canonical copy constructor
+{
+    std::cout << "[Cat]: Copy constructor called" << std::endl;
+    *this = fx;
+    return ;
+}
+
+Cat & Cat::operator=(const Cat & fx) //canonical copy assignment operator overload
+{
+    std::cout << "[Cat]: Copy assignment operator overload" << std::endl;
+    this->_type = fx.getType();
+    //this->setType(fx.getType());
+    return (*this);
+}
+
+Cat::~Cat(void)                   //canonical destructor
+{
+    std::cout << "[Cat]: Destructor called" << std::endl;
+    return ;
+}
+
+void    Cat::makeSound(void) const
+{
+    std::cout << this->_type << "'s sound is Meooow! Meooow! Meooow!" << std::endl;
+}
