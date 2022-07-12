bd87c4a591988d8f5f4a45bceea1ed8e919549c4
thi-phng <thi-phng@student.42.fr>
2022-07-12 12:57:51 +0200

    brain.cpp adding

--- C04/ex01/Brain.cpp
+++ C04/ex01/Brain.cpp
@@ -3,11 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   Brain.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thi-phng <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/12 12:06:10 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/07/12 12:06:11 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/07/12 12:57:38 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Brain.hpp"
 
+Brain::Brain(void)
+{
+    std::cout << "[Brain]: Canonical Default constructor called" << std::endl;
+    return ;
+}
+
+Brain::Brain(Brain const & fx)
+{
+    std::cout << "[Brain]: Canonical copy constructor called" << std::endl;
+    return ;
+}
+
+Brain::Brain & operator=(const Brain & fx);
+virtual ~Brain(void);
+
+std::string     _ideas[100];
