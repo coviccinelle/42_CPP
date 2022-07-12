feb1711ecddbd68a014dfd32d039f582e1742678
thi-phng <thi-phng@student.42.fr>
2022-07-12 12:19:34 +0200

    added brain in makefile and copied main from sujet

--- C04/ex01/main.cpp
+++ C04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 18:12:00 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/07/12 11:55:06 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/07/12 12:16:42 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,37 +19,36 @@
 
 int	main()
 {
-    const Animal* meta = new Animal();
+    // const Animal* meta = new Animal();
+    // const Animal* j = new Dog();
+    // const Animal* i = new Cat();
+    
+    // std::cout << j->getType() << " " << std::endl;
+    // std::cout << i->getType() << " " << std::endl;
+    
+    // i->makeSound(); //will output the cat sound!
+    // j->makeSound();
+    
+    // meta->makeSound();
+
+    
+    // //...
+
+    // std::cout << std::endl << "Here we go the [Wrong Cat] and [Wrong Animal]" << std::endl << std::endl;
+    // const WrongAnimal       *beta = new WrongAnimal();
+    // const WrongAnimal       *k = new WrongCat();
+
+    // std::cout << k->getType() << " " << std::endl;
+    
+    // k->makeSound(); //will output the cat sound!
+    
+    // beta->makeSound();
+
+
     const Animal* j = new Dog();
     const Animal* i = new Cat();
-    
-    std::cout << j->getType() << " " << std::endl;
-    std::cout << i->getType() << " " << std::endl;
-    
-    i->makeSound(); //will output the cat sound!
-    j->makeSound();
-    
-    meta->makeSound();
-
-    
-    //...
-
-    std::cout << std::endl << "Here we go the [Wrong Cat] and [Wrong Animal]" << std::endl << std::endl;
-    const WrongAnimal       *beta = new WrongAnimal();
-    const WrongAnimal       *k = new WrongCat();
-
-    std::cout << k->getType() << " " << std::endl;
-    
-    k->makeSound(); //will output the cat sound!
-    
-    beta->makeSound();
-
-    std::cout << std::endl << "Destructor section!!!" << std::endl << std::endl;
-    delete meta;
-    delete j;
+    delete j;//should not create a leak
     delete i;
-    delete beta;
-    delete k;
-
+    //...
     return 0;
 }
