d90c3e3a2cfbee64cdb86385db16c88e8fe7b412
thi-phng <thi-phng@student.42.fr>
2022-07-12 12:45:20 +0200

    added Brain.hpp

--- C04/ex01/Brain.hpp
+++ C04/ex01/Brain.hpp
@@ -3,11 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   Brain.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thi-phng <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/12 12:06:00 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/07/12 12:06:01 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/07/12 12:44:56 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
+#pragma once
+#include <iostream>
+
+class Brain
+{
+    public:
+        Brain(void);
+        Brain(Brain const & fx);
+        Brain & operator=(const Brain & fx);
+        ~Brain(void);
+    
+    private:
+        std::string     _ideas[100];
+};
