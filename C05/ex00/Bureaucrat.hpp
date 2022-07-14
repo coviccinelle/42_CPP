1a465bd388cf8b4be84b08778f47afc4899b77c4
thi-phng <thi-phng@student.42.fr>
2022-07-14 13:59:23 +0200

    doing hpp of Bureaucrat

--- C05/ex00/Bureaucrat.hpp
+++ C05/ex00/Bureaucrat.hpp
@@ -3,11 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thi-phng <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/14 13:44:33 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/07/14 13:44:35 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/07/14 13:58:31 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
 
+#include "iostream"
+
+class Bureaucrat
+{
+    Bureaucrat(void);                               //default
+    Bureaucrat(std::string name, int grade);
+    Bureaucrat(Bureaucrat const & fx);              //copy
+    Bureaucrat & operator=(const Bureaucrat & fx);  //overload
+
+    std::string     getName(void)const;
+    unsigned int    getGrade(void)const;
+
+    void            setGrade(unsigned int grade);
+
+    
+    ~Bureaucrat(void);                              //destructor
+};
