106c71499199d60647c9005b3b583e11a48f9552
thi-phng <thi-phng@student.42.fr>
2022-07-14 15:50:15 +0200

    doing cpp Bureaucrat

--- C05/ex00/Bureaucrat.cpp
+++ C05/ex00/Bureaucrat.cpp
@@ -3,11 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thi-phng <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/14 13:44:21 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/07/14 13:44:22 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/07/14 14:42:41 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Bureaucrat.hpp"
 
+Bureaucrat::Bureaucrat(void)
+{
+    std::cout << "[Bureaucrat]: Default constructor called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(Bureaucrat const & fx)
+{
+    std::cout << "[Bureaucrat]: Copy constructor called" << std::endl;
+}
+
+Bureaucrat & Bureaucrat::operator=()
