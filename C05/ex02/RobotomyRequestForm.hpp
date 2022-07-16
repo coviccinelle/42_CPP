f0e9e526a43bb090c117d2f2d6a725527bcfe437
thi-phng <thi-phng@student.42.fr>
2022-07-16 22:09:55 +0200

    RobotomyRequestForm.hpp kind of done

--- C05/ex02/RobotomyRequestForm.hpp
+++ C05/ex02/RobotomyRequestForm.hpp
@@ -3,11 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   RobotomyRequestForm.hpp                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thi-phng <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/16 19:00:43 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/07/16 19:00:44 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/07/16 22:07:42 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
+#pragma once
+
+#include <stdexcept>
+#include <iostream>
+#include "form.hpp"
+
+class Form;
+
+class RobotomyRequestForm : public Form
+{
+    public:
+        RobotomyRequestForm(void); //default
+        RobotomyRequestForm(std::string target);
+        RobotomyRequestForm(std::string name, unsigned int grade);
+        RobotomyRequestForm(RobotomyRequestForm const & fx); //copie
+        
+        RobotomyRequestForm &operator=(const RobotomyRequestForm & fx); // overload
+        void                execute(Bureaucrat const &executor) const;
+
+        std::string         getTarget(void) const;
+
+        ~RobotomyRequestForm(void);
+        
+    protected:
+    private:
+        std::string         _Target;
+
+};
