e711869e5fa7626b4dbb9b28e58d94688b880d3b
thi-phng <thi-phng@student.42.fr>
2022-07-16 22:15:36 +0200

    ShrubberyCreationForm.hpp kind of done

--- C05/ex02/ShrubberyCreationForm.hpp
+++ C05/ex02/ShrubberyCreationForm.hpp
@@ -3,11 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   ShrubberyCreationForm.hpp                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thi-phng <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/16 19:00:19 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/07/16 19:00:20 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/07/16 22:15:12 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
 
+#include <stdexcept>
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include "Form.hpp"
+
+class Form;
+
+class ShrubberyCreationForm
+{
+    public:
+        ShrubberyCreationForm(void); //default
+        ShrubberyCreationForm(std::string target);
+        ShrubberyCreationForm(std::string name, unsigned int grade);
+        ShrubberyCreationForm(ShrubberyCreationForm const & fx); // copy
+        
+        ShrubberyCreationForm &operator= (const ShrubberyCreationForm & fx); //overload
+        void                  execute(Bureaucrat const &executor) const;
+
+        std::string           getTarget(void)const;
+        
+        ~ShrubberyCreationForm(void);
+        
+    protected:
+    private:
+        std::string             _Target;
+};
