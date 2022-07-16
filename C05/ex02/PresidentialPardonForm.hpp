d6b57e5d5085ace992b24fc49cee6e809e51cb14
thi-phng <thi-phng@student.42.fr>
2022-07-16 20:24:27 +0200

    adding Presidential

--- C05/ex02/PresidentialPardonForm.hpp
+++ C05/ex02/PresidentialPardonForm.hpp
@@ -3,11 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   PresidentialPardonForm.hpp                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thi-phng <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/16 19:01:13 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/07/16 19:01:16 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/07/16 20:09:35 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
+#pragma once
+
+#include <stdexcept>
+#include <iostream>
+#include "Form.hpp"
+
+class Form;
+
+class PresidentialPardonForm : public Form
