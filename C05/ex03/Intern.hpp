eb48addc2962487379916f76cee9abf4523cf1d9
thi-phng <thi-phng@student.42.fr>
2022-07-19 12:10:28 +0200

    Intern.hpp  uki

--- C05/ex03/Intern.hpp
+++ C05/ex03/Intern.hpp
@@ -3,11 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   Intern.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thi-phng <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/18 14:20:46 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/07/18 14:20:49 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/07/19 12:03:24 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
 
+#include "Form.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
+#include <stdexcept>
+#include <iostream>
+
+class ShrubberyCreationForm;
+class RobotomyRequestForm;
+class PresidentialPardonForm;
+
+class Intern
+{
+    typedef void (Form:::*ft_ptr)(void); //definition a new type of variable
+    public:
+        Intern(void);              // default
+        Intern(Intern const & fx); // copie
+
+
+        Intern & operator=(const Intern & fx); //overload
+        Form    * makeForm(std::string nameForm, std::string targetForm);
+        
+        ~Intern(void); // destructor
+        
+    protected:
+    private:
+};
+
+std::ostream & operator << (std::ostream & sorie, const Intern & fx);
