72a5672dccc5b726777e50619157d99bfffc0524
thi-phng <thi-phng@student.42.fr>
2022-07-20 15:52:56 +0200

    exclued literal/float and double

--- C06/ex00/main.cpp
+++ C06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 11:00:31 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/07/20 11:13:44 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/07/20 15:50:35 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int main(int ac, char **av)
         std::cout << "Invalid arguments, only one argument accepted!" << std::endl;
         return (1);
     }
-    // we create a class where there is a char/int/float/double/double literal/float literal
+    // we create a class where there is a char/int/float/double
     // We create an instance
     Convert convert(av[1]);
     // Now send the arg to a function of our instance -> show it
