6601550372336923ce83592c58d634497073fc2a
thi-phng <thi-phng@student.42.fr>
2022-07-21 21:54:14 +0200

    base cpp added

--- C06/ex02/Base.cpp
+++ C06/ex02/Base.cpp
@@ -3,11 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   Base.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thi-phng <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 21:46:20 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/07/21 21:46:21 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/07/21 21:53:54 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Base.hpp"
 
+Base::Base(void)
+{
+    std::cout << "[Base]: Constructor called" << std::endl;
+}
+
+Base::~Base(void)
+{
+    std::cout << "[Base]: Destructor called" << std::endl;
+}
