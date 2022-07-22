eb4fdf03a2a888fe579e0f3e8505ef0dadb7314f
thi-phng <thi-phng@student.42.fr>
2022-07-22 21:38:50 +0200

    euh ex01

--- C07/ex01/iter.hpp
+++ C07/ex01/iter.hpp
@@ -6,11 +6,25 @@
 /*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 21:32:55 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/07/22 21:33:27 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/07/22 21:38:36 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-#include <iostream>
+template <typename T>
+void    my_function(T const & arg)
+{
+    std::cout << "my_function" << static_cast<T>(arg) << std::endl;
+}
 
+template <typename T>
+void iter(T *t, const int size, void(*f)(T const & arg))
+{
+    int i = 0;
+    while (i < size)
+    {
+        (f)(t[i]);
+        i++;
+    }
+}
