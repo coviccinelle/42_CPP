5eeed04abdcc5949b4c9d5890cd0948eba0cc076
thi-phng <thi-phng@student.42.fr>
2022-07-22 21:27:50 +0200

    whatever.h

--- C07/ex00/whatever.hpp
+++ C07/ex00/whatever.hpp
@@ -3,11 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   whatever.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thi-phng <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 21:19:31 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/07/22 21:19:32 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/07/22 21:27:36 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
 
+#include <iostream>
+
+template <typename T>
+void    swap(T & am T & b)
+{
+    T temp;
+    
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+template <typename T>
+T & min(T & a, T & b)
+{
+    if (a < b)
+        return (a);
+    else
+        return (b);
+}
+
+template <typename T>
+T & max(T &a, T & b)
+{
+    if (a > b)
+        return (a);
+    else
+        return (b);
+}
