2e27cb97f0fe7258230d27b152e652568ba00a35
thi-phng <thi-phng@student.42.fr>
2022-07-23 21:53:35 +0200

    hpp array

--- C07/ex02/Array.hpp
+++ C07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 21:47:18 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/07/23 12:16:42 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/07/23 21:53:06 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,52 @@ template <typename T>
 
 class Array
 {
+    class   out_of_range: public std::exception
+    {
+        public:
+            virtual const char * what() const throw()
+            {
+                return ("Array index is out of range \n");
+            }
+    };
+    
     public:
-        Array(/* args */);
-        ~Array();
+        Array(void);
+        Array(unsigned int n); //param constructor
+        Array(Array const & fx); //copy constructor
+
+        Array   & operator=(const Array & fx); //copy assignment operator overload
+        T       & operator[](unsigned int pos)const; // array subscripting operator
+        unsigned int    size(void) const;
+        {
+            return (this->_size); // -> get size
+        }
+        virtual ~Array(void);
+        
     private:
-        /* data */
+        unsigned int    _size;
+        T               * array;
 };
+
+
+template<typename T>
+Array<T>::Array(void) : _size(0), array(NULL)// default constructor
+{
+    std::cout << "[Array]: Default constructor" << std::endl;
+}
+
+template<typename T>
+Array<T>::Array(unsigned int n) : _size(n), array(new T[_size])// create new tab
+{
+    for (unsigned int i = 0; i < this->_size; i++)
+        this->array[i] = T();
+    std::cout << "[Array]: Array parametre constructor" << std::endl;
+}
+
+template<typename T>
+Array<T>::Array(Array const & fx) // copy constructor
+{
+    std::cout << "[Array]: Copy constructor" << std::endl;
+    this->_size = fx.size();
+    this->array = new T(fx._size);
+}
--- C07/ex02/main.cpp
+++ C07/ex02/main.cpp
@@ -6,60 +6,111 @@
 /*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 21:46:06 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/07/22 21:46:35 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/07/23 20:53:45 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
-#include <Array.hpp>
+#include "Array.hpp"
+#include <stdexcept>
+#include <cstdlib>
 
 #define MAX_VAL 750
-int main(int, char**)
+// int main(int, char**)
+// {
+//     Array<int> numbers(MAX_VAL);
+//     int* mirror = new int[MAX_VAL];
+//     srand(time(NULL));
+//     for (int i = 0; i < MAX_VAL; i++)
+//     {
+//         const int value = rand();
+//         numbers[i] = value;
+//         mirror[i] = value;
+//     }
+//     //SCOPE
+//     {
+//         Array<int> tmp = numbers;
+//         Array<int> test(tmp);
+//     }
+
+//     for (int i = 0; i < MAX_VAL; i++)
+//     {
+//         if (mirror[i] != numbers[i])
+//         {
+//             std::cerr << "didn't save the same value!!" << std::endl;
+//             return 1;
+//         }
+//     }
+//     try
+//     {
+//         numbers[-2] = 0;
+//     }
+//     catch(const std::exception& e)
+//     {
+//         std::cerr << e.what() << '\n';
+//     }
+//     try
+//     {
+//         numbers[MAX_VAL] = 0;
+//     }
+//     catch(const std::exception& e)
+//     {
+//         std::cerr << e.what() << '\n';
+//     }
+
+//     for (int i = 0; i < MAX_VAL; i++)
+//     {
+//         numbers[i] = rand();
+//     }
+//     delete [] mirror;//
+//     return 0;
+// }
+
+int main (void)
 {
-    Array<int> numbers(MAX_VAL);
-    int* mirror = new int[MAX_VAL];
-    srand(time(NULL));
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        const int value = rand();
-        numbers[i] = value;
-        mirror[i] = value;
-    }
-    //SCOPE
-    {
-        Array<int> tmp = numbers;
-        Array<int> test(tmp);
-    }
+	std::cout << ".........Int........." << std::endl;
+	Array<int>	intArray(3);
+	intArray[0] = 10;
+	intArray[1] = 20;
+	intArray[2] = 30;
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        if (mirror[i] != numbers[i])
-        {
-            std::cerr << "didn't save the same value!!" << std::endl;
-            return 1;
-        }
-    }
-    try
-    {
-        numbers[-2] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    try
-    {
-        numbers[MAX_VAL] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        numbers[i] = rand();
-    }
-    delete [] mirror;//
-    return 0;
+	std::cout << intArray << std::endl;
+	Array<int> intcopy(intArray);
+	std::cout << intcopy << std::endl;
+	
+	intArray[0] = 1;
+	intArray[1] = 2;
+	intArray[2] = 3;
+	try{
+		intArray[10] = 4;
+	}
+	catch (std::exception & e) {
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << intArray << std::endl;
+	Array<int>	intArray2;
+	intArray2 = intArray;
+	std::cout << intArray2 << std::endl;
+
+
+	std::cout << std::endl << ".........String........." << std::endl;
+	Array<std::string> stringArray(5);
+	stringArray[0] = "qwerty";
+	stringArray[1] = "azerty";
+	stringArray[2] = "Bonjour";
+	stringArray[3] = "Hello";
+	stringArray[4] = "Hola";
+	try{
+		stringArray[45] = "Aloha";
+	}
+	catch (std::exception & e ){
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << stringArray << std::endl;
+	
+	return (0);
 }
+
+// https://www.youtube.com/watch?v=ZudCUY2oNKg
+// Tuto qui m'a permis de faire une classe template
