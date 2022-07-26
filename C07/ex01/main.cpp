d4c95a88101b64e76a48e62d4abfa963e2639c45
thi-phng <thi-phng@student.42.fr>
2022-07-26 11:23:49 +0200

    added more example to ex01

--- C07/ex01/main.cpp
+++ C07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 21:32:40 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/07/25 22:00:53 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/07/26 11:23:01 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,9 @@
 int main(void)
 {
     int tab[3] = {3, 4, 5};
+    std::string tab2[4] = {"hello", "coucou", "welcome", "home"};
 
     iter(tab, 3, my_function);
+    iter(tab2, 4, my_function);
     return (0);
 }
