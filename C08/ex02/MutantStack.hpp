b4ca295e87f473cdf780e4bef0df6f9c6c28a9ef
thi-phng <thi-phng@student.42.fr>
2022-07-26 21:57:25 +0200

    kind of done ex02 CPP08, need to do more main ex02

--- C08/ex02/MutantStack.hpp
+++ C08/ex02/MutantStack.hpp
@@ -3,11 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   MutantStack.hpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thi-phng <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/26 21:43:17 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/07/26 21:43:18 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/07/26 21:54:37 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
+#pragma once
+
+#include <iostream>
+#include <stack>
+
+// https://stackoverflow.com/questions/525365/does-stdstack-expose-iterators
+// how to use existed objet in class template of stack
+
+template<typename T>
+class MutantStack : public std::stack<T>
+{
+    public:
+        MutantStack(){}//constructor
+        MutantStack(MutantStack<T> const & fx){this->c = fx.c;}
+        //a protected object in the container stack that allow us to acces to deque -> iterator of deque
+        
+        //*** Functions ***//
+        MutantStack<T> & operator=(const MutantStack<T> & fx){this->c = fx.c;}
+
+        ~MutantStack(){}
+
+        typedef typename std::stack<T>::container_type:: iterator iterator;
+        iterator    begin() {return this->c.begin(); }
+        iterator    end() {return this->c.end(); }
+        
+    private:  
+};
--- C08/ex02/main.cpp
+++ C08/ex02/main.cpp
@@ -3,11 +3,99 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thi-phng <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/26 21:42:45 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/07/26 21:42:46 by thi-phng         ###   ########.fr       */
+/*   Updated: 2022/07/26 21:56:37 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "MutantStack.hpp"
+#include <list>
 
+// int main()
+// {
+//     MutantStack<int> mstack;
+//     mstack.push(5);
+//     mstack.push(17);
+    
+//     std::cout << mstack.top() << std::endl;
+//     mstack.pop();
+    
+//     std::cout << mstack.size() << std::endl;
+//     mstack.push(3);
+//     mstack.push(5);
+//     mstack.push(737);
+//     //[...]
+    
+//     mstack.push(0);
+//     MutantStack<int>::iterator it = mstack.begin();
+//     MutantStack<int>::iterator ite = mstack.end();
+//     ++it;
+//     --it;
+    
+//     while (it != ite)
+//     {
+//         std::cout << *it << std::endl;
+//         ++it;
+//     }
+//     std::stack<int> s(mstack);
+//     return 0;
+// }
+
+int main()
+{
+	std::cout << ".........stack........." << std::endl;
+	
+	{
+		MutantStack<int> mstack;
+
+		mstack.push(5);
+		mstack.push(17);
+		std::cout << "The top of the elements is " << mstack.top() << std::endl;
+
+		mstack.pop();
+		std::cout << "The size is " << mstack.size() << std::endl;
+
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		mstack.push(0);
+
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::stack<int> s(mstack);
+	}
+
+	std::cout << ".........list........." << std::endl;
+
+	{
+		std::list<int> mstack;
+
+		mstack.push_back(5);
+		mstack.push_back(17);
+		std::cout << "The last element is " << mstack.back() << std::endl;
+
+		mstack.pop_back();
+		std::cout << "The size is = " << mstack.size() << std::endl;
+
+		mstack.push_back(3);
+		mstack.push_back(5);
+		mstack.push_back(737);
+		mstack.push_back(0);
+
+		std::list<int>::iterator it = mstack.begin();
+		std::list<int>::iterator ite = mstack.end();
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+	}
+	return 0;
+}
