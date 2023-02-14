8e102d316fdec007f9a3186ed0326bf7bc0c00ac
coviccinelle_mac <thi-phng@student.42.fr>
2023-02-14 15:12:53 +0100

    using iterator in megaphone CPP 00 instead of a loop for each char

--- C00/corrected/ex00/megaphone.cpp
+++ C00/corrected/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: thi-phng <thi-phng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 12:51:49 by thi-phng          #+#    #+#             */
-/*   Updated: 2022/03/17 13:42:32 by thi-phng         ###   ########.fr       */
+/*   Updated: 2023/02/14 15:11:54 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,11 @@ int main(int ac, char **av)
 	int i = 1;
 	while (i < ac)
 	{
-		int j = 0;
-		while (av[i][j])
-		{
-			if (av[i][j] >= 'a' && av[i][j] <= 'z')
-				av[i][j] -= 32;
-			j++;
-		}
-		std::cout << av[i];
+		std::string av1[i];
+		av1[i] = av[i];
+		// using toupper + iterator to go from begin to end of the string, without using a loop like in C
+		std::transform(av1[i].begin(), av1[i].end(), av1[i].begin(), ::toupper);
+		std::cout << av1[i];
 		std::cout << " ";
 		i++;
 	}
